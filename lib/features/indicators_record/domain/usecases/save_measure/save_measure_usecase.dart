26bb01f187620fd5bd198967aee934e9afcc171b
tortamque <90132962+tortamque@users.noreply.github.com>
2024-02-07 20:21:13 +0200

    Rewrite use cases

--- lib/features/indicators_record/domain/usecases/save_measure/save_measure_usecase.dart
+++ lib/features/indicators_record/domain/usecases/save_measure/save_measure_usecase.dart
@@ -2,7 +2,7 @@ import 'package:bloodque/core/shared/domain/repository/measures_repository.dart'
 import 'package:bloodque/core/usecases/usecase.dart';
 import 'package:bloodque/features/indicators_record/domain/usecases/save_measure/save_measure_params.dart';
 
-class SaveMeasureUseCase implements UseCase<void, SaveMeasureParams>{
+class SaveMeasureUseCase implements UseCase<Future<void>, SaveMeasureParams>{
   SaveMeasureUseCase(this._repository);
 
   final MeasuresRepository _repository;
