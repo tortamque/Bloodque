9342e40c100da0cee30b6ab7e855d30628f5cd2d
tortamque <90132962+tortamque@users.noreply.github.com>
2024-02-07 15:04:08 +0200

    Create mesuare entity

--- lib/core/shared/domain/entities/measure_entity.dart
+++ lib/core/shared/domain/entities/measure_entity.dart
@@ -1,12 +1,21 @@
 import 'package:equatable/equatable.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
+part 'measure_entity.g.dart';
+
+@HiveType(typeId: 1)
 class MeasureEntity extends Equatable {
   const MeasureEntity({required this.systolic, required this.diastolic, required this.pulse, required this.time, required this.date});
 
+  @HiveField(0)
   final int systolic;
+  @HiveField(1)
   final int diastolic;
+  @HiveField(2)
   final int pulse;
+  @HiveField(3)
   final DateTime time;
+  @HiveField(4)
   final DateTime date;
   
   @override
---
+++ lib/core/shared/domain/entities/measure_entity.g.dart
@@ -0,0 +1,53 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'measure_entity.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class MeasureEntityAdapter extends TypeAdapter<MeasureEntity> {
+  @override
+  final int typeId = 1;
+
+  @override
+  MeasureEntity read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return MeasureEntity(
+      systolic: fields[0] as int,
+      diastolic: fields[1] as int,
+      pulse: fields[2] as int,
+      time: fields[3] as DateTime,
+      date: fields[4] as DateTime,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, MeasureEntity obj) {
+    writer
+      ..writeByte(5)
+      ..writeByte(0)
+      ..write(obj.systolic)
+      ..writeByte(1)
+      ..write(obj.diastolic)
+      ..writeByte(2)
+      ..write(obj.pulse)
+      ..writeByte(3)
+      ..write(obj.time)
+      ..writeByte(4)
+      ..write(obj.date);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is MeasureEntityAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
