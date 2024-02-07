21c24606f2e30f73d2b4ec6976aa62307d93dcf8
tortamque <90132962+tortamque@users.noreply.github.com>
2024-02-07 17:59:09 +0200

    Move event

--- lib/core/shared/presentation/bloc/measures_bloc.dart
+++ lib/core/shared/presentation/bloc/measures_bloc.dart
@@ -4,7 +4,7 @@ import 'package:bloodque/core/shared/presentation/bloc/measures_event.dart';
 import 'package:bloodque/core/shared/presentation/bloc/measures_state.dart';
 import 'package:bloodque/features/indicators_record/domain/usecases/save_measure/save_measure_params.dart';
 import 'package:bloodque/features/indicators_record/domain/usecases/save_measure/save_measure_usecase.dart';
-import 'package:bloodque/features/indicators_record/presentation/bloc/save_measure_event.dart';
+import 'package:bloodque/features/indicators_record/presentation/bloc/save_measure_bloc/save_measure_event.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class MeasuresBloc extends Bloc<MeasuresEvent, MeasuresState> {
