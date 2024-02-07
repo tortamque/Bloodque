6c25df5fc385d56b43e0e9fda46168bf2e7e4e08
tortamque <90132962+tortamque@users.noreply.github.com>
2024-02-07 22:50:05 +0200

    Add MeasureCard widget

--- lib/features/view/presentation/pages/main_view_page.dart
+++ lib/features/view/presentation/pages/main_view_page.dart
@@ -5,9 +5,9 @@ import 'package:bloodque/core/shared/presentation/widgets/custom_app_bar.dart';
 import 'package:bloodque/features/view/presentation/bloc/get_measures_bloc/get_measures_bloc.dart';
 import 'package:bloodque/features/view/presentation/bloc/get_measures_bloc/get_measures_event.dart';
 import 'package:bloodque/features/view/presentation/bloc/get_measures_bloc/get_measures_state.dart';
+import 'package:bloodque/features/view/presentation/widgets/measure_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:intl/intl.dart';
 
 class MainViewPage extends StatefulWidget {
   const MainViewPage({super.key});
@@ -27,12 +27,7 @@ class _MainViewPageState extends State<MainViewPage> {
   Widget build(BuildContext context) {
     return BlocBuilder<GetMeasuresBloc, GetMeasuresState>(
         builder: (context, state) {
-          if (state is GetMeasuresLoading) {
-            return const Scaffold(
-              appBar: CustomAppBar(title: 'Blood Pressure BPM Tracker'),
-              body: Center(child: CircularProgressIndicator()),
-            );
-          } else if (state is GetMeasuresStateDone) {
+          if (state is GetMeasuresStateDone) {
             return Scaffold(
               appBar: const CustomAppBar(title: 'Blood Pressure BPM Tracker'),
               floatingActionButton: FloatingActionButton(
@@ -43,86 +38,28 @@ class _MainViewPageState extends State<MainViewPage> {
                 },
                 child: const Icon(Icons.add),
               ),
-              body: ListView.builder(
-                itemCount: state.measures?.length ?? 0,
-                itemBuilder: (context, index) {
-                  final measure = state.measures?[index];
-    
-                  return Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 3.5),
-                    child: Card(
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 7.5),
-                        child: Row(
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.symmetric(horizontal: 10),
-                              child: Column(
-                                children: [
-                                  Text(
-                                    '${measure!.systolic}',
-                                    style: const TextStyle(
-                                      fontSize: 23,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                  Text(
-                                    '${measure.diastolic}',
-                                    style: const TextStyle(
-                                      fontSize: 23,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                ]
-                              ),
-                            ),
-                      
-                            Container(
-                              decoration: const BoxDecoration(
-                                color: greenColor,
-                                borderRadius: BorderRadius.all(Radius.circular(20)),
-                              ),
-                              
-                              width: 7.5,
-                              height: MediaQuery.of(context).size.height / 10,
-                            ),
-                  
-                            Padding(
-                              padding: const EdgeInsets.symmetric(horizontal: 10),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    '${measure.time.hour}:${measure.time.minute < 10 ? '0${measure.time.minute}' : measure.time.minute}, ${DateFormat('dd/MM/yyyy').format(measure.date)}',
-                                    style: TextStyle(
-                                      fontSize: 23,
-                                      color: Colors.grey[600]
-                                    ),
-                                  ),
-                                  Text(
-                                    'Pulse: ${measure.pulse} BPM',
-                                    style: TextStyle(
-                                      fontSize: 23,
-                                      color: Colors.grey[600]
-                                    )
-                                  ),
-                                ],
-                              ),
-                            )
-                          ],
-                        ),
-                      ),
-                    ),
-                  );
-                },
-              ),
+              body: state.measures == null 
+                ? const Center(child: Text('No measures'))
+                : ListView.builder(
+                  itemCount: state.measures?.length ?? 0,
+                  itemBuilder: (context, index) {
+                    final measure = state.measures![index];
+      
+                    return MeasureCard(measure: measure);
+                  },
+                ),
             );
-          } else {
+          } else if (state is GetMeasuresLoading) {
             return const Scaffold(
               appBar: CustomAppBar(title: 'Blood Pressure BPM Tracker'),
-              body: Center(child: Text('No measures')),
+              body: Center(child: CircularProgressIndicator()),
             );
           }
+
+          return const Scaffold(
+            appBar: CustomAppBar(title: 'Blood Pressure BPM Tracker'),
+            body: Center(child: Text('Error')),
+          );
         },
       );
   }
