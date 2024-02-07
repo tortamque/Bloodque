c1b6875c98d96ad5235b7b124416986ba4174e4f
tortamque <90132962+tortamque@users.noreply.github.com>
2024-02-07 13:50:06 +0200

    Init hive

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,16 @@ import 'package:bloodque/config/themes/app_theme.dart';
 import 'package:bloodque/config/themes/colors.dart';
 import 'package:bloodque/features/indicators_record/presentation/pages/indicators_record_page.dart';
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
-  runApp(const MainApp());
+void main() async {
+  await Hive.initFlutter();
+
+  runApp(const BloodqueApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class BloodqueApp extends StatelessWidget {
+  const BloodqueApp({super.key});
 
   @override
   Widget build(BuildContext context) => MaterialApp(
