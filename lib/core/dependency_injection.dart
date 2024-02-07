46698c348d1a62fe7369e883be4f4eebf8be477a
tortamque <90132962+tortamque@users.noreply.github.com>
2024-02-07 15:38:15 +0200

    Update DI

--- lib/core/dependency_injection.dart
+++ lib/core/dependency_injection.dart
@@ -4,6 +4,7 @@ import 'package:bloodque/core/shared/data/models/measure_model.dart';
 import 'package:bloodque/core/shared/data/repository/measures_repository_impl.dart';
 import 'package:bloodque/core/shared/domain/entities/measure_entity.dart';
 import 'package:bloodque/core/shared/domain/repository/measures_repository.dart';
+import 'package:bloodque/features/indicators_record/domain/usecases/save_measure/save_measure_usecase.dart';
 import 'package:get_it/get_it.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
@@ -22,7 +23,10 @@ Future<void> initializeDependencies() async {
     ..registerSingleton<DatabaseService>(DatabaseServiceImpl(getIt()))
 
     // Repository
-    ..registerSingleton<MeasuresRepository>(MeasuresRepositoryImpl(getIt()));
+    ..registerSingleton<MeasuresRepository>(MeasuresRepositoryImpl(getIt()))
+
+    // Use Cases
+    ..registerSingleton<SaveMeasureUseCase>(SaveMeasureUseCase(getIt()));
 }
 
 Future<void> _initializeHive() async {
