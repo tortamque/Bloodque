a2e6a9b7fe95ce35e5be9b492854f01351a2a49f
tortamque <90132962+tortamque@users.noreply.github.com>
2024-02-07 11:16:15 +0200

    Create appbar

--- lib/features/indicators_record/presentation/pages/indicators_record_page.dart
+++ lib/features/indicators_record/presentation/pages/indicators_record_page.dart
@@ -1,10 +1,14 @@
+import 'package:bloodque/core/shared/presentation/widgets/custom_app_bar.dart';
 import 'package:flutter/material.dart';
 
 class IndicatorsRecordPage extends StatelessWidget {
   const IndicatorsRecordPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
+  Widget build(BuildContext context) => const Scaffold(
+    appBar: CustomAppBar(title: 'New Record'),
+    body: Center(
+      child: Text('Indicators Record Page'),
+    ),
+  );
 }
