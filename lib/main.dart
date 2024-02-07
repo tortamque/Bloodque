2dcc20c5335a9c1a6e5feb172dd5b5f5e5063756
tortamque <90132962+tortamque@users.noreply.github.com>
2024-02-07 15:03:40 +0200

    Init DI

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:bloodque/config/themes/app_theme.dart';
 import 'package:bloodque/config/themes/colors.dart';
+import 'package:bloodque/core/dependency_injection.dart';
 import 'package:bloodque/features/indicators_record/presentation/pages/indicators_record_page.dart';
 import 'package:flutter/material.dart';
-import 'package:hive_flutter/hive_flutter.dart';
 
 void main() async {
-  await Hive.initFlutter();
-
+  await initializeDependencies();
+  
   runApp(const BloodqueApp());
 }
 
