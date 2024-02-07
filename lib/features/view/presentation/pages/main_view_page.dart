6d470ba70691248634bb2b77572e652387f24d5c
tortamque <90132962+tortamque@users.noreply.github.com>
2024-02-07 21:00:33 +0200

    Create main page template

--- lib/features/view/presentation/pages/main_view_page.dart
+++ lib/features/view/presentation/pages/main_view_page.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_expression_function_bodies
+
 import 'package:bloodque/core/shared/presentation/bloc/measures_bloc.dart';
 import 'package:bloodque/core/shared/presentation/bloc/measures_state.dart';
 import 'package:bloodque/core/shared/presentation/widgets/custom_app_bar.dart';
@@ -17,20 +19,40 @@ class _MainViewPageState extends State<MainViewPage> {
   @override
   void initState() {
     super.initState();
-    BlocProvider.of<MeasuresBloc>(context).add(GetMeasuresEvent()); // Add the Bloc event in initState
+    BlocProvider.of<MeasuresBloc>(context).add(GetMeasuresEvent()); 
   }
 
   @override
-  Widget build(BuildContext context) => BlocBuilder<MeasuresBloc, MeasuresState>(
-    builder: (context, state) {
-      if(state is GetMeasuresState){
-        print(state.measures?.length ?? 0);
-        return Scaffold(
-          appBar: const CustomAppBar(title: 'Blood Pressure BPM Tracker'),
-          body: Container(),
-        );
-      }
-      return Placeholder();
-    },
-  );
+  Widget build(BuildContext context) {
+    return BlocBuilder<MeasuresBloc, MeasuresState>(
+      builder: (context, state) {
+        if (state is MeasuresLoading) {
+          return const Scaffold(
+            appBar: CustomAppBar(title: 'Blood Pressure BPM Tracker'),
+            body: Center(child: CircularProgressIndicator()),
+          );
+        } else if (state is GetMeasuresState) {
+          return Scaffold(
+            appBar: const CustomAppBar(title: 'Blood Pressure BPM Tracker'),
+            body: ListView.builder(
+              itemCount: state.measures?.length ?? 0,
+              itemBuilder: (context, index) {
+                final measure = state.measures?[index];
+
+                return ListTile(
+                  title: Text(measure?.systolic.toString() ?? ''),
+                  subtitle: Text(measure?.diastolic.toString() ?? ''),
+                );
+              },
+            ),
+          );
+        } else {
+          return const Scaffold(
+            appBar: CustomAppBar(title: 'Blood Pressure BPM Tracker'),
+            body: Center(child: Text('No measures')),
+          );
+        }
+      },
+    );
+  }
 }
