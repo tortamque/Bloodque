a009e9f1ab5785d83ca9b6bba124812ad6af236b
tortamque <90132962+tortamque@users.noreply.github.com>
2024-02-07 19:02:12 +0200

    Add blocbuilder

--- lib/features/indicators_record/presentation/widgets/save_button/save_button.dart
+++ lib/features/indicators_record/presentation/widgets/save_button/save_button.dart
@@ -13,54 +13,60 @@ class SaveButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => BlocBuilder<MeasuresBloc, MeasuresState>(
-    builder: (context, state) {
-      return BlocBuilder<ChangeMeasureBloc, ChangeMeasureState>(
+    builder: (context, state) => BlocBuilder<ChangeMeasureBloc, ChangeMeasureState>(
       builder: (context, state) => Padding(
         padding: const EdgeInsets.symmetric(vertical: 10),
-        child: SizedBox(
-          width: MediaQuery.of(context).size.width,
-          child: TextButton(
-            onPressed: () {
-              context.read<MeasuresBloc>().add(
-                SaveMeasureEvent(
-                  measure: MeasureEntity(
-                    systolic: state.systolic,
-                    diastolic: state.diastolic,
-                    pulse: state.pulse,
-                    date: state.date,
-                    time: DateTime(
-                      state.date.year,
-                      state.date.month,
-                      state.date.day,
-                      state.time.hour,
-                      state.time.minute,
+        child: BlocBuilder<MeasuresBloc, MeasuresState>(
+          builder: (context, measuresState) {
+            if(measuresState is MeasuresLoading){
+              return const CircularProgressIndicator();
+            } else{
+              return SizedBox(
+                width: MediaQuery.of(context).size.width,
+                child: TextButton(
+                  onPressed: () {
+                    context.read<MeasuresBloc>().add(
+                      SaveMeasureEvent(
+                        measure: MeasureEntity(
+                          systolic: state.systolic,
+                          diastolic: state.diastolic,
+                          pulse: state.pulse,
+                          date: state.date,
+                          time: DateTime(
+                            state.date.year,
+                            state.date.month,
+                            state.date.day,
+                            state.time.hour,
+                            state.time.minute,
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color?>(mainColor),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12.5),
+                      ),
+                    ),
+                  ),
+                  child: const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 7.5),
+                    child: Text(
+                      'Save',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                      ),
                     ),
                   ),
                 ),
               );
-            },
-            style: ButtonStyle(
-              backgroundColor: MaterialStateProperty.all<Color?>(mainColor),
-              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12.5),
-                ),
-              ),
-            ),
-            child: const Padding(
-              padding: EdgeInsets.symmetric(vertical: 7.5),
-              child: Text(
-                'Save',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
-                ),
-              ),
-            ),
-          ),
+            }
+          },
         ),
       ),
-    );
-    },
+    ),
   );
 }
