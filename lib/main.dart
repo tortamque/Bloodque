abfed7d79da3f707b59638b66f3d0f5421065c7a
tortamque <90132962+tortamque@users.noreply.github.com>
2024-02-08 09:52:17 +0200

    Init navigator

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:bloodque/config/themes/app_theme.dart';
 import 'package:bloodque/config/themes/colors.dart';
+import 'package:bloodque/core/constants/navigator_constants.dart';
 import 'package:bloodque/core/dependency_injection.dart';
 import 'package:bloodque/features/indicators_record/presentation/bloc/change_measure_bloc/change_measure_bloc.dart';
 import 'package:bloodque/features/indicators_record/presentation/bloc/save_measure_bloc/save_measures_bloc.dart';
+import 'package:bloodque/features/indicators_record/presentation/pages/indicators_record_page.dart';
 import 'package:bloodque/features/view/presentation/bloc/get_measures_bloc/get_measures_bloc.dart';
 import 'package:bloodque/features/view/presentation/pages/detailed_view_page.dart';
+import 'package:bloodque/features/view/presentation/pages/main_view_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -27,7 +30,12 @@ class BloodqueApp extends StatelessWidget {
     ],
     child: MaterialApp(
       theme: theme(mainColor),
-      home: DetailedViewPage(),
+      initialRoute: mainPageConstant,
+      routes: {
+        mainPageConstant: (context) => const MainViewPage(),
+        detailedPageConstant: (context) => const DetailedViewPage(),
+        recordPageConstant: (context) => const IndicatorsRecordPage(),
+      },
     ),
   );
 }
